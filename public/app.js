baaed9f03bce98c5df5866639ca2253fd3fe8638
Yash! <chachadyash786@gmail.com>
2020-01-26 13:46:08 +0530

    Linking basic done

--- public/app.js
+++ public/app.js
@@ -4,6 +4,7 @@ var cout_date = document.getElementById("cout-date")
 var adults = document.getElementById("adults")
 var child = document.getElementById("child")
 var submit = document.getElementById("submit")
+var resultsList = document.querySelector("#results")
 const form = document.querySelector('#form');
 
 form.addEventListener("submit",getResults)
@@ -17,17 +18,29 @@ function getResults(event){
     console.log(adults.value)
     console.log(child.value)
 
-    
-   // const {username , room } = Qs.parse(location.search, {ignoreQueryPrefix : true })
-
-   // console.log(location.search)
 
     const u = "http://localhost:3000/hotels/"+loc.value+"/"+cin_date.value+"/"+cout_date.value
-
+    var result = document.querySelector(".result")
     fetch(u)
         .then(response=>{
             response.json().then(data=>{
                 console.log(data)
+                data.map(result=>{  
+                    const li = document.createElement('li')
+                    const img = document.createElement('img')
+                    const moreDet =  document.createElement('a')
+                    img.src=result.image
+                    moreDet.href =  result.link
+                    moreDet.textContent = "Know More"
+
+                    li.textContent=result.name +"          Rs."+result.price
+                    li.appendChild(img)
+                    li.appendChild(moreDet)
+
+                    
+
+                    resultsList.appendChild(li)
+                })
             })
         })
 
